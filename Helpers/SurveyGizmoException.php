f77a3f124a3751aee08c560a2650c89c7b03ccf4
SurveyGizmo Development <SurveyGizmoOfficial@users.noreply.github.com>
2016-05-13 13:24:10 -0400

    added auth check and account model

--- Helpers/SurveyGizmoException.php
+++ Helpers/SurveyGizmoException.php
@@ -1,12 +1,37 @@
 <?php namespace SurveyGizmo;
-class SurveyGizmoException extends Exception{
-	
-	function __construct($type,$message){
-			
-	}	
+class SurveyGizmoException{
 
-	private function processException(){
+	const NOT_IMPLEMENTED  = 404;
+    const NOT_SUPPORTED  = 405;
+    const NOT_AUTHORIZED = 401;
 
-	}
+	function __construct($type = null) {
+		$this->type = $type;
+    }
+
+    function getMessage(){
+    	$type = $this->type;
+    	$response = new Response();
+		$response->results = false;
+		$message = 'An Error has Occurred.';
+
+        switch ($type) {
+            case self::NOT_IMPLEMENTED:
+                $message = 'Method not implemented';
+                break;
+            case self::NOT_SUPPORTED:
+                $message = 'Method not supported';
+                break;
+            case self::NOT_AUTHORIZED:
+	            $message = 'Not Authorized';
+            break;
+            default: 
+            	$type = 500;
+                break;
+        }
+        $response->code = $type;
+		$response->message = $message;
+		return $response;
+    }
 }
 ?>
