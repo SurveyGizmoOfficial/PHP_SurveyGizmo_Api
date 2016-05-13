f2d2972b4c5cfaf739e174d5f84fff1c09be8a27
Erik <erik@code.boulder.sgizmo.com>
2016-05-13 15:39:49 -0400

    test base

---
+++ Tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+namespace SurveyGizmo;
+require_once(__DIR__ . '/../SurveyGizmoAutoLoader.php');
+/**
+ * test cases base class
+ */
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+}
