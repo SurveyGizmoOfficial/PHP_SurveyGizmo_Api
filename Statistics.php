f79a56d4e0fab9d36e6c8a5c8411c0b66a00f27e
SurveyGizmo Development <SurveyGizmoOfficial@users.noreply.github.com>
2016-05-20 16:00:08 -0400

    new file

---
+++ Statistics.php
@@ -0,0 +1,14 @@
+<?php
+namespace SurveyGizmo;
+use SurveyGizmo\ApiResource;
+
+class Statistics extends ApiResource {
+
+	static $path = "/survey/{survey_id}/surveystatistic";
+
+	public static function get()
+	{
+	}
+
+}
+?>
