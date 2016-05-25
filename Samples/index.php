0d9ef1c703095eda799002b557ae7073795f01c7
SurveyGizmo Development <SurveyGizmoOfficial@users.noreply.github.com>
2016-05-25 14:25:22 -0400

    cleanup sample script

--- Samples/index.php
+++ Samples/index.php
@@ -1,45 +1,44 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', 1);
-function testLog($message, $dump = null){
-	echo "<h3>". $message . "</h3>";
-	if($dump){
-		testDump($dump);
-	}
+function testLog($message, $dump = null)
+{
+    echo "<h3>" . $message . "</h3>";
+    if ($dump) {
+        testVardump($dump);
+    }
 }
-function testDump($dump){
-	echo "<pre>";
-	var_dump($dump);
-	echo "</pre>";
+function testVardump($dump)
+{
+    echo "<pre>";
+    var_dump($dump);
+    echo "</pre>";
 }
 testLog("TESTING Library");
 //Require autoloader
 require_once "../SurveyGizmoAutoLoader.php";
 //set token & secret
-$api_key = "e0f82ba3c6c1e769ad3eb8eb4a4457e2d3085a86ef3739b134";
-$api_secret = "A9Nx6SYE7dr56";
+$api_key = "";
+$api_secret = "";
 
 //authetnicate
 testLog("Authenticating");
 
 try {
-	$sg = SurveyGizmo\SurveyGizmoAPI::auth($api_key,$api_secret);
+    $sg = SurveyGizmo\SurveyGizmoAPI::auth($api_key, $api_secret);
 } catch (Exception $e) {
-	testLog("Error Authenticating", $e);
-	die;
+    testLog("Error Authenticating", $e);
+    die;
 }
-// if($sg != true){
-// 	testLog("Error Authenticating",$sg);
-// 	die;
-// }
+
 // $filter = new SurveyGizmo\Helpers\Filter();
 // $fiterItem = new SurveyGizmo\Helpers\FilterItem('field','=','x');
 // $filter->add($filterItem);
 
 // testLog("Getting surveys with paging");
-// testDump(SurveyGizmo\Survey::fetch(null, array(
-// 	'page' => 3,
-// 	'limit' => 100
+// testVardump(SurveyGizmo\Survey::fetch(null, array(
+//     'page' => 3,
+//     'limit' => 100
 // )));
 
 testLog("Getting Surveys");
@@ -60,14 +59,14 @@ testLog("Updating Survey " . $sid);
 $survey->title = "TEST from API";
 $survey->save();
 
-$sid = 1035186;
+$sid = 123;
 testLog("Getting Survey " . $sid);
 $survey = SurveyGizmo\Resources\Survey::get($sid);
 // testLog("got Survey",$survey);
-$campaign = $survey->getCampaign(140308);
+$campaign = $survey->getCampaign(123);
 
 testLog("got Campaign", $campaign);
-$email = $campaign->getEmailMessage(128073);
+$email = $campaign->getEmailMessage(123);
 testLog("got Email Message", $email);
 
 $question = $survey->getQuestion(10);
@@ -91,7 +90,7 @@ $reports = $survey->getReports();
 // testLog("got Reports ",$reports);
 
 testLog("Getting one report for survey " . $sid);
-$report = SurveyGizmo\Resources\Survey\Report::get($sid, 450421);
+$report = SurveyGizmo\Resources\Survey\Report::get($sid, 123);
 // testLog("got report",$report);
 
 testLog("Updating Survey Page 1 title for survey " . $sid);
@@ -103,41 +102,40 @@ testLog("Creating a new survey");
 $survey = new SurveyGizmo\Resources\Survey();
 $survey->title = "NEW FROM API";
 $results = $survey->save();
-testLog("Survey created",$results->data);
+testLog("Survey created", $results->data);
 
 testLog("Deleting our new survey " . $survey->id);
 $results = $survey->delete();
-testDump($results);
+testVardump($results);
 
 testLog("Getting account");
 $account = SurveyGizmo\Resources\Account::get();
-testDump($account);
+testVardump($account);
 
 // testLog("Getting account users");
 // $response = SurveyGizmo\Resources\User::fetch();
-// testDump($response);
+// testVardump($response);
 
 // testLog("Getting one user");
-// $user = SurveyGizmo\Resources\User::get(126904);
-// testDump($user);
+// $user = SurveyGizmo\Resources\User::get(123);
+// testVardump($user);
 
 // $user = new SurveyGizmo\Resources\User();
-// $user->email = 'garrett+apitest@sgizmo.com';
+// $user->email = 'test@email.com';
 // $user->password = '123qwe';
-// $user->username = 'Works?';
-// $user->team = 223388;
+// $user->username = 'Username';
+// $user->team = 123;
 // var_dump($user->save());
 
 // testLog("Getting teams");
-// $team = SurveyGizmo\Resources\Team::get(224885);
-// testDump(SurveyGizmo\Resources\Team::fetch());
-// testDump($team);
+// $team = SurveyGizmo\Resources\Team::get(123);
+// testVardump(SurveyGizmo\Resources\Team::fetch());
+// testVardump($team);
 // $team->delete();
+
 // $team = new SurveyGizmo\Resources\Team();
 // $team = SurveyGizmo\Resources\Team::get(235681);
 // $team->teamname = 'Team Awesome';
 // $team->description = 'Team Awesome is awesome too';
-// testDump($team);
+// testVardump($team);
 // $team->save();
-
-?>
